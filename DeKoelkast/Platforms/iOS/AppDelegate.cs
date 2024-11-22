44f98480e7cd03a89ed97d31ab83c885650bff2a
Nessen03 <115788592+Nessen03@users.noreply.github.com>
2024-11-22 14:06:19 +0100

    Add project files.

---
+++ DeKoelkast/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DeKoelkast
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

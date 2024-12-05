e9981be7920cccb7cc641f2f841f871e93404395
Sen <115788592+ichishihoness@users.noreply.github.com>
2024-12-05 15:40:39 +0100

    Adjusted product page frontend and bit of backend

--- DeKoelkast/ProductPage.xaml.cs
+++ DeKoelkast/ProductPage.xaml.cs
@@ -3,10 +3,35 @@ namespace DeKoelkast;
 public partial class ProductPage : ContentPage
 {
     private MVVM.Models.Products _product;
+
     public ProductPage(MVVM.Models.Products product)
-	{
+    {
+        InitializeComponent();
         _product = product;
         BindingContext = _product;
-        InitializeComponent();
-	}
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (Changeiconlabel != null)
+        {
+            Console.WriteLine($"Changeiconlabel.Text: {Changeiconlabel.Text}"); // Debug statement
+
+            if (Changeiconlabel.Text == "greybottleicon.png")
+            {
+                ProductIcon.Source = "bottleicon.png";
+            }
+            else
+            {
+                ProductIcon.Source = "canicon.png";
+            }
+        }
+        else
+        {
+            ProductIcon.Source = "settingsicon.png";
+        }
+    }
 }
+
---
+++ DeKoelkast/ProductSettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DeKoelkast;
+
+public partial class ProductSettingsPage : ContentPage
+{
+	public ProductSettingsPage()
+	{
+		InitializeComponent();
+	}
+}
